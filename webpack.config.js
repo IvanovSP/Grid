352f51456c485b49c1d4db36bb133bf3b42ab1d1
sergeyivanov <sergeyivanov@cloud.upwork.com>
2018-02-15 12:26:39 +0200

    Basic project

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+	context: path.join(__dirname, 'src'),
+	devtool:  'inline-sourcemap' ,
+	entry: './js/client.js',
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['react', 'es2015'],
+					plugins: ['react-html-attrs', 'transform-class-properties']
+				}
+			}
+		]
+	},
+	output: {
+		path: __dirname + '/src/',
+		filename: 'client.min.js'
+	},
+	plugins: []
+};
